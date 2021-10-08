bca7628526376cbada67a2008f03bc03aee08cd0
Kirans87 <kiranstark07@gmail.com>
2021-10-08 12:01:38 +0530

    push

---
+++ Gitpractice/src/test/java/gitpractice1/test1.java
@@ -0,0 +1,9 @@
+package gitpractice1;
+
+public class test1 {
+	
+	public static void main(String[] args) {
+		System.out.println("hi i am Kiran S");
+	}
+
+}
---
+++ Gitpractice/src/test/java/gitpractice2/test2.java
@@ -0,0 +1,9 @@
+package gitpractice2;
+
+public class test2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("new push");
+	}
+
+}
