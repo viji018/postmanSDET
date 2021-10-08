82df3295c397ee8a154a11a4176d131bd59cc897
viji018 <jayavg18@gmail.com>
2021-10-08 12:45:47 +0530

    hi

--- Gitpractice/src/test/java/gitpractice1/test1.java
+++ Gitpractice/src/test/java/gitpractice1/test1.java
@@ -4,7 +4,7 @@ public class test1 {
 	
 	public static void main(String[] args) {
 		System.out.println("hi i am Kiran S");
-		System.out.println("i m vijay");
+		System.out.println("i m vijay 1");
 	}
 
 }
